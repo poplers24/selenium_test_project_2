d9e28c334fc8726f3cfe1f6c89e5ba9582b37b40
Maxim S <poplers24@gmail.com>
2023-01-06 20:22:08 +0300

    adding a payment page and a finish page, a screenshot script in the base page, and completing the first test

--- base/base_class.py
+++ base/base_class.py
@@ -1,3 +1,4 @@
+import datetime
 
 class Base():
     def __init__(self, driver):
@@ -17,4 +18,17 @@ class Base():
         assert value_word == result
         print("Good value word")
 
+    """Method Screenshot"""
+
+    def get_screenshot(self):
+        now_date = datetime.datetime.now().strftime("%H.%M.%S7%Y.%m.%d")
+        name_screenshot = 'screenshot_' + now_date + '.png'
+        self.driver.save_screenshot('/Users/Maksim/Desktop/Python_auto/selenium_test_project_2/screen/' + name_screenshot)
+
+    """Method assert url"""
+
+    def assert_url(self, result):
+        get_url = self.driver.current_url
+        assert get_url == result
+        print("Good value url")
 
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -3,8 +3,11 @@ import time
 from selenium import webdriver
 
 from pages.cart_page import Cart_page
+from pages.client_information_page import Client_information_page
+from pages.finish_page import Finish_page
 from pages.login_page import Login_page
 from pages.main_page import Main_page
+from pages.payment_page import Payment_page
 
 
 def test_buy_product():
@@ -14,10 +17,21 @@ def test_buy_product():
 
     login = Login_page(driver)
     login.authorization()
+
     mp = Main_page(driver)
     mp.select_product()
+
     cp = Cart_page(driver)
     cp.click_checkout_button()
+
+    cip = Client_information_page(driver)
+    cip.input_information()
+
+    p = Payment_page(driver)
+    p.payment()
+
+    f = Finish_page(driver)
+    f.finish()
 
     time.sleep(10)
     driver.quit()
